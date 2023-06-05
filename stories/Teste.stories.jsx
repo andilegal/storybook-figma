87494d8d90fcb15335ae210fd9d4cc4de25df82d
Anderson Batista da Costa <48361666+andilegal@users.noreply.github.com>
2023-06-05 20:24:07 -0300

    Add files via upload

---
+++ stories/Teste.stories.jsx
@@ -0,0 +1,23 @@
+import { withDesign } from 'storybook-addon-designs';
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'FigmaExample',
+  component: 'MyComponent',
+  decorators: [withDesign],
+  
+};
+
+export default meta;
+
+export const mystory = ()=> <button>anders</button>
+
+
+  mystory.parameters = {
+    design: {
+      type: 'figspec',
+      url: "https://www.figma.com/file/rugFzpoJjnRlKHeeA9uQGS/Auditoria?type=design&node-id=37-1521&t=IdLkJZQma3bWeAry-4",
+      accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE'
+    },
+  }
