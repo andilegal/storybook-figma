42ca7af7f7c9ecb31f0944c223ac550e035739e6
abcosta <abcosta@recordtv.com.br>
2023-06-16 16:28:19 -0300

    update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook-addon-designs"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook-addon-designs", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/nextjs",
     options: {}
--- stories/Teste.stories.jsx
+++ stories/Teste.stories.jsx
@@ -1,6 +1,5 @@
 import { withDesign } from 'storybook-addon-designs';
 
-
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'FigmaExample',
@@ -11,13 +10,39 @@ const meta = {
 
 export default meta;
 
-export const mystory = ()=> <button>anders</button>
-
+export const mystory = () => <button>anders</button>
+export const mystory2 = ()=> <button>anders</button>
+export const mystory3 = ()=> <button>anders</button>
+export const mystory4 = ()=> <button>anders</button>
 
 mystory.parameters = {
   design: {
     type: 'figspec',
     url: "https://www.figma.com/file/rugFzpoJjnRlKHeeA9uQGS/Auditoria?type=design&node-id=85-2807&t=F5PPWcfPqJeqx8f8-4",
+    accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE'
+  },
+}
+
+mystory2.parameters = {
+  design: {
+    type: 'figma',
+    url: "https://www.figma.com/proto/rugFzpoJjnRlKHeeA9uQGS/Auditoria?page-id=101%3A3013&type=design&node-id=102-3179&scaling=min-zoom&starting-point-node-id=102%3A3179",
+    accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE'
+  },
+}
+
+mystory3.parameters = {
+  design: {
+    type: 'figma',
+    url: "https://www.figma.com/file/rugFzpoJjnRlKHeeA9uQGS/Auditoria?type=design&node-id=17-2&t=truWvqsajkrVkUVW-0",
+    accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE'
+  },
+}
+
+mystory4.parameters = {
+  design: {
+    type: 'figma',
+    url: "https://www.figma.com/file/rugFzpoJjnRlKHeeA9uQGS/Auditoria?type=design&node-id=35-1518&t=truWvqsajkrVkUVW-0",
     accessToken: 'figd_sHs6Ap894w4C-OAFls7tuq0fMMROyue-8zQJ8hRE'
   },
 }
